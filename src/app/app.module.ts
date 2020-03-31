9b2ac2e94e9d3c2e574844c9ad02d92dd08e9be4
Sergiy <48173627+SergiyMolchan@users.noreply.github.com>
2020-03-31 19:34:33 +0300

    create appbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppbarComponent } from './appbar/appbar.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppbarComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
