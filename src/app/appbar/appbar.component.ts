dfc9f59b616a78d780bda69f67bf706fd53e5f1b
Sergiy <48173627+SergiyMolchan@users.noreply.github.com>
2020-04-25 15:47:16 +0300

    app bar update

--- src/app/appbar/appbar.component.ts
+++ src/app/appbar/appbar.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, OnInit} from '@angular/core';
+import {AuthorizationService} from '../authorization/authorization.service';
 
 @Component({
   selector: 'app-appbar',
   templateUrl: './appbar.component.html',
-  styleUrls: ['./appbar.component.sass']
+  styleUrls: ['./appbar.component.sass'],
+  changeDetection: ChangeDetectionStrategy.Default
 })
-export class AppbarComponent implements OnInit {
+export class AppbarComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  constructor(
+    private authorizationService: AuthorizationService,
+  ) { }
 
-  ngOnInit(): void {
-  }
+  @Input() isAuth: boolean;
+  logout: any = () => this.authorizationService.logout();
+
+  ngOnChanges() {}
+
+  ngOnInit(): void {}
 
 }
